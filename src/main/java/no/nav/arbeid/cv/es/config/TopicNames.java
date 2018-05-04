421f4e0540ffc49ad4206d163f2a58658d4827ba
Tore Engvig <Tore.Engvig@nav.no>
2018-05-04 11:42:10 +0200

    Fjernet DLT

--- src/main/java/no/nav/arbeid/cv/es/config/TopicNames.java
+++ src/main/java/no/nav/arbeid/cv/es/config/TopicNames.java
@@ -3,5 +3,4 @@ package no.nav.arbeid.cv.es.config;
 public class TopicNames {
 
   public static final String TOPIC_CVEVENT_V3 = "no.nav.arbeid.pam.cv.cvevent.3";
-  public static final String TOPIC_CVEVENT_V3_FEIL = "no.nav.arbeid.pam.cv.cvevent.3.dlt";
 }
