8ca5f7bac9936c1b0c91150b1238058a8bf7713e
Tore Engvig <Tore.Engvig@nav.no>
2018-05-07 15:51:58 +0200

    Endret litt på helsesjekkene

---
+++ src/test/java/no/nav/arbeid/cv/es/DockerComposeTest.java
@@ -0,0 +1,19 @@
+package no.nav.arbeid.cv.es;
+
+import com.palantir.docker.compose.DockerComposeRule;
+import no.nav.arbeid.cv.es.es.CvListenerTest;
+import no.nav.arbeid.cv.es.es.IndexCvTest;
+import no.nav.arbeid.cv.es.rest.SearchControllerTest;
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({CvListenerTest.class, IndexCvTest.class, SearchControllerTest.class})
+public class DockerComposeTestSuite {
+    @ClassRule
+    public static DockerComposeRule docker =
+            DockerComposeRule.builder().file("src/test/resources/docker-compose-kafka-og-es.yml")
+                    .build();
+
+}
